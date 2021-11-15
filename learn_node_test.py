8d27bff49a60960f0e37a7240bc47a6b02144862
KimJinHyeon0 <rlawlsgus117@gmail.com>
2021-11-16 00:30:32 +0900

    [ADD] LSTM class / prediction variables / saving function

--- learn_node_test.py
+++ learn_node_test.py
@@ -1,4 +1,12 @@
 """Unified interface to all dynamic graph model experiments"""
+
+'''
+class imbalance test
+
+adjusted amount of training graph.
+random sampled majority classes with num of minority class's graph 
+'''
+
 import math
 import logging
 import time
@@ -10,31 +18,23 @@ from tqdm import tqdm
 import torch
 import pandas as pd
 import numpy as np
-from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_auc_score, average_precision_score, f1_score, recall_score
+from collections import Counter
 
 from module import TGAN
 from graph import NeighborFinder
 
-from collections import Counter
 
-class LR(torch.nn.Module):
-    def __init__(self, dim, drop=0.3, agg='mean'):
+
+class MEAN(torch.nn.Module):
+    def __init__(self, feat_dim, drop=0.3):
         super().__init__()
 
-        self.fc_1 = torch.nn.Linear(dim, 80)
+        self.fc_1 = torch.nn.Linear(feat_dim, 80)
         self.fc_2 = torch.nn.Linear(80, 10)
         self.fc_3 = torch.nn.Linear(10, 1)
         self.act = torch.nn.ReLU()
         self.dropout = torch.nn.Dropout(p=drop, inplace=True)
-
-
-        # if agg == 'lstm':
-        #     self.logger.info('Aggregation uses LSTM model')
-        # elif agg == 'mean':
-        #     self.logger.info('Aggregation uses constant mean model')
-        # else:
-        #     raise ValueError('invalid agg_method value, use attn or lstm')
-
 
     def forward(self, x):
         x = x.mean(dim=0)
@@ -44,6 +44,49 @@ class LR(torch.nn.Module):
         x = self.dropout(x)
         return self.fc_3(x)
 
+class LSTM(torch.nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim, n_layer, bidirectional, fc_dim, drop=0.3):
+        super().__init__()
+
+        self.output_dim = output_dim
+        self.num_layers = n_layer
+        self.input_size = input_dim
+        self.hidden_dim = hidden_dim
+        self.bidirectional = bidirectional
+
+        self.lstm = torch.nn.LSTM(input_size=self.input_size,
+                                  hidden_size=self.hidden_dim,
+                                  num_layers=self.num_layers,
+                                  bidirectional = self.bidirectional)
+
+        if bidirectional:
+            self.fc_1 = torch.nn.Linear(self.hidden_dim * 2, fc_dim)
+
+        else:
+            self.fc_1 = torch.nn.Linear(self.hidden_dim, fc_dim)
+
+
+        self.fc_2 = torch.nn.Linear(fc_dim, self.output_dim)
+        self.act = torch.nn.ReLU()
+        self.dropout = torch.nn.Dropout(drop)
+
+    def forward(self, emb):
+
+        p2d = (0, 0, 0, MAX_SEQ - emb.shape[0])
+        embedded = torch.nn.functional.pad(emb, p2d)
+        embedded = torch.unsqueeze(embedded, 1)
+
+        output, (h_out, c_out) = self.lstm(embedded)
+
+        if self.bidirectional:
+            h_out = self.dropout(torch.cat((h_out[-2, :, :], h_out[-1, :, :]), dim=1))
+        else:
+            h_out = self.dropout(h_out[-1, :, :])
+
+        h_out = self.act(self.fc_1(h_out))
+        h_out = self.dropout(h_out)
+
+        return self.fc_2(h_out).squeeze(1)
 
 random.seed(222)
 np.random.seed(222)
@@ -51,7 +94,7 @@ torch.manual_seed(222)
 
 ### Argument and global variables
 parser = argparse.ArgumentParser('Interface for TGAT experiments on node classification')
-parser.add_argument('-d', '--data', type=str, help='data sources to use, try wikipedia or reddit', default='iama')
+parser.add_argument('-d', '--data', type=str, help='data sources to use, try wikipedia or reddit', default='showerthoughts')
 parser.add_argument('--bs', type=int, default=30, help='batch_size')
 parser.add_argument('--prefix', type=str, default='')
 parser.add_argument('--n_degree', type=int, default=50, help='number of neighbors to sample')
@@ -59,7 +102,7 @@ parser.add_argument('--n_neg', type=int, default=1)
 parser.add_argument('--n_head', type=int, default=2)
 parser.add_argument('--n_epoch', type=int, default=15, help='number of epochs')
 parser.add_argument('--n_layer', type=int, default=2)
-parser.add_argument('--lr', type=float, default=3e-4)
+parser.add_argument('--lr', type=float, default=1e-4)
 parser.add_argument('--tune', action='store_true', help='parameters tunning mode, use train-test split on training data only.')
 parser.add_argument('--drop_out', type=float, default=0.1, help='dropout probability')
 parser.add_argument('--gpu', type=int, default=0, help='idx for the gpu to use')
@@ -91,12 +134,31 @@ USE_TIME = args.time
 AGG_METHOD = args.agg_method
 ATTN_MODE = args.attn_mode
 SEQ_LEN = NUM_NEIGHBORS
-DATA = args.data
 NUM_LAYER = args.n_layer
 LEARNING_RATE = args.lr
 NODE_LAYER = 1
 NODE_DIM = args.node_dim
 TIME_DIM = args.time_dim
+
+#############################
+DATA = args.data
+CLASS_BALANCING = 'BALANCED'
+PRED_METHOD = 'LSTM'
+TRAINING_METHOD = 'SELECTIVE'
+
+hidden_dim = 128
+fc_dim = 40
+output_dim = 1
+n_layer = 1
+bidirectional = False
+############################
+
+try:
+    saved_model = pd.read_csv('./saved_models/model_perfomance_eval.csv', index_col=0)
+    MODEL_NUM = saved_model.index[-1]+1
+except:
+    MODEL_NUM = 0
+
 
 ### set up logger
 logging.basicConfig(level=logging.INFO)
@@ -119,8 +181,8 @@ e_feat = np.load('./processed/{}_edge_feat.npy'.format(DATA))
 n_feat = np.load('./processed/{}_node_feat.npy'.format(DATA))
 
 train_time = 3888000
-
-time_cut = 86400
+tgat_time_cut = 7200
+time_cut = 7200
 
 g_num = g_df.g_num.values
 g_ts = g_df.g_ts.values
@@ -129,8 +191,6 @@ dst_l = g_df.i.values
 e_idx_l = g_df.idx.values
 label_l = g_df.label.values
 ts_l = g_df.ts.values
-
-
 
 max_src_index = src_l.max()
 max_idx = max(src_l.max(), dst_l.max())
@@ -154,7 +214,8 @@ test_ts_l = ts_l[test_flag]
 test_e_idx_l = e_idx_l[test_flag]
 test_label_l = label_l[test_flag]
 
-
+temp = g_df[ts_l < time_cut].g_num.values
+MAX_SEQ = Counter(temp).most_common(1)[0][1]
 
 ### Initialize the data structure for graph and edge sampling
 adj_list = [[] for _ in range(max_idx + 1)]
@@ -184,13 +245,17 @@ logger.debug('num of training instances: {}'.format(num_instance))
 logger.debug('num of graphs per epoch: {}'.format(len(set(train_g_num_l))))
 
 logger.info('loading saved TGAN model')
-model_path = f'./saved_models/{args.prefix}-{args.agg_method}-{args.attn_mode}-{DATA}.pth'
+#model_path = f'./saved_models/{MODEL_NUM}-TGAT.pth'
+model_path = f'./saved_models/6-TGAT.pth'
 tgan.load_state_dict(torch.load(model_path))
 tgan.eval()
 logger.info('TGAN models loaded')
-logger.info('Start training node classification task')
+logger.info('model num: {}'.format(MODEL_NUM))
+logger.info('Start training Graph classification task')
 
-lr_model = LR(n_feat.shape[1])
+lr_model = LSTM(n_feat.shape[1], hidden_dim, output_dim, n_layer, bidirectional, fc_dim)
+#lr_model = MEAN(n_feat.shape[1])
+
 lr_model = lr_model.to(device)
 lr_optimizer = torch.optim.Adam(lr_model.parameters(), lr=args.lr)
 tgan.ngh_finder = full_ngh_finder
@@ -199,27 +264,21 @@ lr_criterion_eval = torch.nn.BCELoss()
 
 def eval_epoch(src_l, ts_l, g_num_l, lr_model, tgan, data_type, num_layer=NODE_LAYER):
 
-    ground_truth = np.zeros(len(set(g_num_l)))
-    pred_prob = np.zeros(len(set(g_num_l)))
+    graph_num = np.array([])
+    raw_edge_len = np.array([])
+    sliced_edge_len = np.array([])
+    true_label = np.array([])
+    pred_prob = np.array([])
+    pred_label = np.array([])
 
     loss = 0
-    num_instance = len(set(g_num_l))
 
     with torch.no_grad():
         lr_model.eval()
         tgan.eval()
 
-        g_num_list = []
-        raw_list = []
-        splitted_list = []
-        label_list = []
-        pred_list = []
-        prob_list = []
-
-        g_list = np.array(list(set(g_num_l)))
-        np.random.shuffle(g_list)
-
         for i, k in enumerate(set(g_num_l)):
+
             temp_src_cut = src_l[g_num_l == k]
             temp_ts_cut = ts_l[g_num_l == k]
 
@@ -228,54 +287,108 @@ def eval_epoch(src_l, ts_l, g_num_l, lr_model, tgan, data_type, num_layer=NODE_L
             src_l_cut = temp_src_cut[valid_flag]
             ts_l_cut = temp_ts_cut[valid_flag]
 
-            label = 0
-            if False in valid_flag:
-                label = 1
+            label = 1 if False in valid_flag else 0
 
-            size = len(src_l_cut)
-
-            if size < 2:
+            if len(src_l_cut) < 2:
                 continue
 
             src_embed = tgan.tem_conv(src_l_cut, ts_l_cut, num_layer)
+
             src_label = torch.tensor([label]).float().to(device)
 
             lr_prob = lr_model(src_embed).sigmoid()
             loss += lr_criterion_eval(lr_prob, src_label).item()
 
-            ground_truth[i] = label
-            pred_prob[i] = lr_prob.cpu().numpy()
+            graph_num = np.append(graph_num, k)
+            raw_edge_len = np.append(raw_edge_len, len(temp_src_cut))
+            sliced_edge_len = np.append(sliced_edge_len, len(src_l_cut))
+            true_label = np.append(true_label, label)
+            pred_prob = np.append(pred_prob, lr_prob.cpu().numpy())
+            pred_label = np.append(pred_label, 1 if lr_prob >= 0.5 else 0)
 
-            g_num_list.append(k)
-            raw_list.append(len(temp_src_cut))
-            splitted_list.append(len(src_l_cut))
-            label_list.append(label)
-            prob_list.append(round(lr_prob.item(), 4))
-            pred_list.append(1 if lr_prob >= 0.5 else 0)
+    label_ratio = sum(true_label)/len(true_label)
+    acc = (true_label == pred_label).mean()
+    auc_roc = roc_auc_score(true_label, pred_prob)
+    AP = average_precision_score(true_label, pred_prob)
+    recall = recall_score(true_label, pred_label)
+    F1 = f1_score(true_label, pred_label)
 
-    auc_roc = roc_auc_score(ground_truth, pred_prob)
 
-    if data_type == 'test':
-        df = pd.DataFrame({'g_num' : g_num_list,
-                           'raw_edge_num' : raw_list,
-                           'sliced_edge_num' : splitted_list,
-                           'label' : label_list,
-                           'prob' : prob_list,
-                           'pred' : pred_list})
+    if data_type:
+        # save performance
 
-        df.to_csv('./saved_data/{}-{}-{}-{}.csv'.format(DATA, time_cut, 'mean', round(auc_roc, 4)))
-        print('df saved')
+        new_model = pd.DataFrame.from_dict([{'SUBREDDIT': DATA,
+                                            'TGAT_TIME_CUT': tgat_time_cut,
+                                            'PRED_TIME_CUT' : time_cut,
+                                            'PRED_METHOD': PRED_METHOD,
+                                            'TRAINING_METHOD': TRAINING_METHOD,
+                                            'CLASS_BALANCING' : CLASS_BALANCING,
+                                            'NUM_GRAPH': len(graph_num),
+                                            'RAW_EDGE_NUM': sum(raw_edge_len),
+                                            'USED_EDGE_NUM': sum(sliced_edge_len),
+                                            'LABEL_RATIO' : label_ratio,
+                                            'ACCURACY' : acc,
+                                            'AUC_ROC_SCORE': auc_roc,
+                                            'AP_SCORE': AP,
+                                            'RECALL_SCORE': recall,
+                                            'F1_SCORE': F1}])
 
-    return auc_roc, loss / num_instance
+        try:
+            saved_model = pd.read_csv('./saved_models/model_perfomance_eval.csv', index_col=0)
+            updated_model = saved_model.append(new_model)
+            updated_model = updated_model.reset_index(drop=True)
+            updated_model.to_csv('./saved_models/model_perfomance_eval.csv')
+        except:
+            new_model.to_csv('./saved_models/model_perfomance_eval.csv')
+
+        #save model details
+        df = pd.DataFrame({'g_num' : graph_num,
+                           'raw_edge_num' : raw_edge_len,
+                           'sliced_edge_num' : sliced_edge_len,
+                           'true_label' : true_label,
+                           'pred_prob' : pred_prob,
+                           'pred_label' : pred_label})
+
+        df.to_csv('./saved_data/{}.csv'.format(MODEL_NUM))
+
+    return acc, auc_roc, AP, recall, F1, loss / num_instance
 
 for epoch in tqdm(range(args.n_epoch)):
     tgan = tgan.eval()
     lr_model = lr_model.train()
 
-    g_list = np.array(list(set(train_g_num_l)))
-    np.random.shuffle(g_list)
+    if CLASS_BALANCING == 'BALANCED':
+        neg_k = np.array([])
+        pos_k = np.array([])
 
-    for k in g_list:
+        for k in set(train_g_num_l):
+            temp_src_cut = train_src_l[train_g_num_l == k]
+            temp_ts_cut = train_ts_l[train_g_num_l == k]
+
+            valid_flag = (temp_ts_cut < time_cut)
+            src_l_cut = temp_src_cut[valid_flag]
+
+            if len(src_l_cut) < 2:
+                continue
+
+            if False in valid_flag:
+                pos_k = np.append(pos_k, k)
+            else:
+                neg_k = np.append(neg_k, k)
+
+        if len(pos_k) > len(neg_k):
+            pos_k = np.random.choice(pos_k, len(neg_k))
+        else:
+            neg_k = np.random.choice(neg_k, len(pos_k))
+
+        g_l = np.concatenate((pos_k, neg_k), axis=0)
+
+    else:
+        g_l = train_g_num_l
+
+    np.random.shuffle(g_l)
+
+    for k in set(g_l):
         temp_src_cut = train_src_l[train_g_num_l == k]
         temp_ts_cut = train_ts_l[train_g_num_l == k]
 
@@ -284,14 +397,7 @@ for epoch in tqdm(range(args.n_epoch)):
         src_l_cut = temp_src_cut[valid_flag]
         ts_l_cut = temp_ts_cut[valid_flag]
 
-        label = 0
-        if False in valid_flag:
-            label = 1
-
-        size = len(src_l_cut)
-
-        if size < 2:
-            continue
+        label = 1 if False in valid_flag else 0
 
         lr_optimizer.zero_grad()
         with torch.no_grad():
@@ -304,11 +410,13 @@ for epoch in tqdm(range(args.n_epoch)):
         lr_loss.backward()
         lr_optimizer.step()
 
-    train_auc, train_loss = eval_epoch(train_src_l, train_ts_l, train_g_num_l, lr_model, tgan, 'train')
-    test_auc, test_loss = eval_epoch(test_src_l, test_ts_l, test_g_num_l, lr_model, tgan, 'train')
-    #torch.save(lr_model.state_dict(), './saved_models/edge_{}_wkiki_node_class.pth'.format(DATA))
-    logger.info(f'train auc: {train_auc}, test auc: {test_auc}')
 
-test_auc, test_loss = eval_epoch(test_src_l, test_ts_l, test_g_num_l, lr_model, tgan, 'test')
-torch.save(lr_model.state_dict(), './saved_models/{}-{}-{}-{}.pth'.format(DATA, time_cut, 'mean', round(test_auc, 4)))
-logger.info(f'test auc: {test_auc}')
+    train_acc, train_auc, train_AP, train_recall, train_F1, train_loss = eval_epoch(train_src_l, train_ts_l, train_g_num_l, lr_model, tgan, 0)
+    test_acc, test_auc, test_AP, test_recall, test_F1, test_loss = eval_epoch(test_src_l, test_ts_l, test_g_num_l, lr_model, tgan, 0)
+    #torch.save(lr_model.state_dict(), './saved_models/edge_{}_wkiki_node_class.pth'.format(DATA))
+    logger.info(f'train acc: {train_acc}, train auc: {train_auc}, train AP: {train_AP}, train Recall_Score: {train_recall}, train F1: {train_F1}')
+    logger.info(f'test acc: {test_acc}, test auc: {test_auc}, test AP: {test_AP}, test Recall_Score: {test_recall}, test F1: {test_F1}')
+
+test_acc, test_auc, test_AP, test_recall, test_F1, test_loss = eval_epoch(test_src_l, test_ts_l, test_g_num_l, lr_model, tgan, 1)
+torch.save(lr_model.state_dict(), './saved_models/{}-{}.pth'.format(MODEL_NUM, 'PREDICT'))
+logger.info(f'test acc: {test_acc}, test auc: {test_auc}, test AP: {test_AP}, test Recall_Score: {test_recall}, test F1: {test_F1}')
