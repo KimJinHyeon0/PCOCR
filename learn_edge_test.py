c23cdbbe0e5b9105cab2d7369225c5b947bc287e
KimJinHyeon0 <rlawlsgus117@gmail.com>
2022-03-05 07:37:45 +0900

    [FIX] SELECTIVE method (class balanced within graph) [FIX] loss criterion (pred_prob, true_label)

--- learn_edge_test.py
+++ learn_edge_test.py
@@ -12,14 +12,13 @@ import numpy as np
 
 from sklearn.metrics import roc_auc_score, average_precision_score, f1_score, recall_score
 
-
 from module import TGAN
 from graph import NeighborFinder
 from utils import EarlyStopMonitor, RandEdgeSampler
 
 ### Argument and global variables
 parser = argparse.ArgumentParser('Interface for TGAT experiments on link predictions')
-parser.add_argument('-d', '--data', type=str, help='data sources to use, try wikipedia or reddit', default='news')
+parser.add_argument('-d', '--data', type=str, help='data sources to use, try wikipedia or reddit', default='iama')
 parser.add_argument('--bs', type=int, default=200, help='batch_size')
 parser.add_argument('--prefix', type=str, default='', help='prefix to name the checkpoints')
 parser.add_argument('--n_degree', type=int, default=20, help='number of neighbors to sample')
@@ -64,17 +63,17 @@ LEARNING_RATE = args.lr
 NODE_DIM = args.node_dim
 TIME_DIM = args.time_dim
 
-TRAINING_METHOD = 'SELECTIVE'
-time_cut = 3900
+TRAINING_METHOD = 'FULL'
+time_cut = 324000
 max_round = 5
 
 MODEL_PERFORMANCE_PATH = f'./saved_models/model_perfomance_eval.csv'
 try:
     saved_model = pd.read_csv(MODEL_PERFORMANCE_PATH, index_col=0)
-    MODEL_NUM = saved_model.index[-1]+1
+    MODEL_NUM = saved_model.index[-1] + 1
 except:
     MODEL_NUM = 0
-
+MODEL_NUM = '000'
 MODEL_SAVE_PATH = f'./saved_models/{MODEL_NUM}-TGAT.pth'
 get_checkpoint_path = lambda \
     epoch: f'./saved_checkpoints/{MODEL_NUM}-TGAT-{epoch}.pth'
@@ -99,7 +98,7 @@ def eval_one_epoch(hint, tgan, sampler, src, dst, ts, label):
     val_acc, val_ap, val_f1, val_auc = [], [], [], []
     with torch.no_grad():
         tgan = tgan.eval()
-        TEST_BATCH_SIZE = 30
+        TEST_BATCH_SIZE = 100
         num_test_instance = len(src)
         num_test_batch = math.ceil(num_test_instance / TEST_BATCH_SIZE)
 
@@ -107,40 +106,29 @@ def eval_one_epoch(hint, tgan, sampler, src, dst, ts, label):
             # percent = 100 * k / num_test_batch
             # if k % int(0.2 * num_test_batch) == 0:
             #     logger.info('{0} progress: {1:10.4f}'.format(hint, percent))
-
             s_idx = k * TEST_BATCH_SIZE
             e_idx = min(num_test_instance - 1, s_idx + TEST_BATCH_SIZE)
             src_l_cut = src[s_idx:e_idx]
             dst_l_cut = dst[s_idx:e_idx]
             ts_l_cut = ts[s_idx:e_idx]
-            # label_l_cut = label[s_idx:e_idx]
+            label_l_cut = label[s_idx:e_idx]
 
-            size = len(src_l_cut)
-            src_l_fake, dst_l_fake = sampler.sample(size)
-
-            pos_prob, neg_prob = tgan.contrast(src_l_cut, dst_l_cut, dst_l_fake, ts_l_cut, NUM_NEIGHBORS)
-
-            pred_score = np.concatenate([(pos_prob).cpu().numpy(), (neg_prob).cpu().numpy()])
-            pred_label = pred_score > 0.5
-            true_label = np.concatenate([np.ones(size), np.zeros(size)])
+            pred_prob = tgan.contrast(src_l_cut, dst_l_cut, ts_l_cut, NUM_NEIGHBORS).cpu().numpy()
+            pred_label = pred_prob > 0.5
+            true_label = label_l_cut
 
             val_acc.append((pred_label == true_label).mean())
-            val_ap.append(average_precision_score(true_label, pred_score))
+            val_ap.append(average_precision_score(true_label, pred_prob))
             val_f1.append(f1_score(true_label, pred_label))
-            val_auc.append(roc_auc_score(true_label, pred_score))
+            val_auc.append(roc_auc_score(true_label, pred_prob))
 
     return np.mean(val_acc), np.mean(val_ap), np.mean(val_f1), np.mean(val_auc)
 
 
 ### Load data and train val test split
-g_df = pd.read_csv('./processed/{}_structure.csv'.format(DATA))
+g_df = pd.read_csv('./processed/{}_structure.csv'.format(DATA), index_col=0)
 e_feat = np.load('./processed/{}_edge_feat.npy'.format(DATA))
 n_feat = np.load('./processed/{}_node_feat.npy'.format(DATA))
-
-
-train_time = 3888000  # '2018-02-15 00:00:00' - '2018-01-01 00:00:00'
-test_time = np.quantile(g_df[g_df['g_ts'] > train_time].g_ts, 0.5)
-
 
 g_num = g_df.g_num.values
 g_ts = g_df.g_ts.values
@@ -150,53 +138,19 @@ ts_l = g_df.ts.values
 label_l = g_df.label.values
 e_idx_l = g_df.idx.values
 
-max_idx = max(src_l.max(), dst_l.max())
-random.seed(2020)
+train_time = 3888000  # '2018-02-15 00:00:00' - '2018-01-01 00:00:00'
+test_time = np.quantile(g_ts[(g_ts > train_time)], 0.5)
 
 train_flag = (g_ts < train_time)
 test_flag = (g_ts >= train_time) & (g_ts < test_time)
 valid_flag = (g_ts >= test_time)
 
-# train_g_num = list(set(g_num[train_flag]))
-# mask_node_set = []
-#
-# for num in train_g_num:
-#     src_list = g_df[g_num == num].u.values
-#     mask_node_set += random.sample(set(src_list), int(0.1 * len(src_list)))
-#
-# mask_src_flag = g_df.u.map(lambda x: x in mask_node_set).values
-#
-# valid_test_flag = test_flag * (1 - mask_src_flag)
-# valid_val_flag = valid_flag * (1 - mask_src_flag)
-#
-# nn_test_flag = test_flag * mask_src_flag
-# nn_val_flag = valid_flag * mask_src_flag
-
-if TRAINING_METHOD == 'SELECTIVE':
-    p_train_g_num_l = g_num[train_flag]
-    p_train_src_l = src_l[train_flag]
-    p_train_dst_l = dst_l[train_flag]
-    p_train_ts_l = ts_l[train_flag]
-    p_train_label_l = label_l[train_flag]
-    p_train_e_idx_l = e_idx_l[train_flag]
-
-    time_cut_flag = (p_train_ts_l < time_cut)
-
-    train_g_num_l = p_train_g_num_l[time_cut_flag]
-    train_src_l = p_train_src_l[time_cut_flag]
-    train_dst_l = p_train_dst_l[time_cut_flag]
-    train_ts_l = p_train_ts_l[time_cut_flag]
-    train_label_l = p_train_label_l[time_cut_flag]
-    train_e_idx_l = p_train_e_idx_l[time_cut_flag]
-
-else:
-    train_g_num_l = g_num[train_flag]
-    train_src_l = src_l[train_flag]
-    train_dst_l = dst_l[train_flag]
-    train_ts_l = ts_l[train_flag]
-    train_label_l = label_l[train_flag]
-    train_e_idx_l = e_idx_l[train_flag]
-
+train_g_num = g_num[train_flag]
+train_src_l = src_l[train_flag]
+train_dst_l = dst_l[train_flag]
+train_ts_l = ts_l[train_flag]
+train_label_l = label_l[train_flag]
+train_e_idx_l = e_idx_l[train_flag]
 
 val_src_l = src_l[valid_flag]
 val_dst_l = dst_l[valid_flag]
@@ -204,32 +158,60 @@ val_ts_l = ts_l[valid_flag]
 val_label_l = label_l[valid_flag]
 val_e_idx_l = e_idx_l[valid_flag]
 
-
 test_src_l = src_l[test_flag]
 test_dst_l = dst_l[test_flag]
 test_ts_l = ts_l[test_flag]
 test_label_l = label_l[test_flag]
 test_e_idx_l = e_idx_l[test_flag]
 
+random.seed(2020)
+logger.info('SUBREDDIT : {}'.format(DATA))
+logger.info('TIME_CUT : {}'.format(time_cut))
+logger.info('TRAINING METHOD : {}'.format(TRAINING_METHOD))
 
-# nn_val_src_l = src_l[nn_val_flag]
-# nn_val_dst_l = dst_l[nn_val_flag]
-# nn_val_ts_l = ts_l[nn_val_flag]
-# nn_val_label_l = label_l[nn_val_flag]
-# nn_val_e_idx_l = e_idx_l[nn_val_flag]
-#
-#
-# nn_test_src_l = src_l[nn_test_flag]
-# nn_test_dst_l = dst_l[nn_test_flag]
-# nn_test_ts_l = ts_l[nn_test_flag]
-# nn_test_label_l = label_l[nn_test_flag]
-# nn_test_e_idx_l = e_idx_l[nn_test_flag]
+if TRAINING_METHOD == 'SELECTIVE':
+    time_cut_flag = (train_ts_l < time_cut)
 
+    train_g_num = train_g_num[time_cut_flag]
+    train_src_l = train_src_l[time_cut_flag]
+    train_dst_l = train_dst_l[time_cut_flag]
+    train_ts_l = train_ts_l[time_cut_flag]
+    train_label_l = train_label_l[time_cut_flag]
+    train_e_idx_l = train_e_idx_l[time_cut_flag]
 
+    train_index_l = np.arange(len(train_g_num))
+    selective_flag = np.zeros((len(train_g_num)), dtype=bool)
+
+    for k in set(train_g_num):
+        temp_flag = (train_g_num == k)
+
+        if sum(temp_flag) < 2:
+            continue
+
+        temp_index = train_index_l[temp_flag]
+        temp_label = train_label_l[temp_flag]
+
+        if temp_label.mean() >= 0.5:
+            major, minor = temp_index[temp_label == 1], temp_index[temp_label == 0]
+
+        else:
+            major, minor = temp_index[temp_label == 0], temp_index[temp_label == 1]
+
+        balanced = np.random.choice(major, len(minor), replace=False)
+        selective_flag[balanced] = True
+        selective_flag[minor] = True
+
+    train_src_l = train_src_l[selective_flag]
+    train_dst_l = train_dst_l[selective_flag]
+    train_ts_l = train_ts_l[selective_flag]
+    train_label_l = train_label_l[selective_flag]
+    train_e_idx_l = train_e_idx_l[selective_flag]
 
 ### Initialize the data structure for graph and edge sampling
 # build the graph for fast query
 # graph only contains the training data (with 10% nodes removal)
+
+max_idx = max(src_l.max(), dst_l.max())
 
 adj_list = [[] for _ in range(max_idx + 1)]
 for src, dst, eidx, ts in zip(train_src_l, train_dst_l, train_e_idx_l, train_ts_l):
@@ -267,7 +249,6 @@ logger.info('num of training instances: {}'.format(num_instance))
 logger.info('num of batches per epoch: {}'.format(num_batch))
 
 idx_list = np.arange(num_instance)  # 0 ~ num_instance
-np.random.shuffle(idx_list)
 early_stopper = EarlyStopMonitor(max_round)
 
 for epoch in range(NUM_EPOCH):  # NUM_EPOCH = 50
@@ -286,19 +267,11 @@ for epoch in range(NUM_EPOCH):  # NUM_EPOCH = 50
         src_l_cut, dst_l_cut = train_src_l[s_idx:e_idx], train_dst_l[s_idx:e_idx]
         ts_l_cut = train_ts_l[s_idx:e_idx]
         label_l_cut = train_label_l[s_idx:e_idx]
-        size = len(src_l_cut)  # size = 200
-        src_l_fake, dst_l_fake = train_rand_sampler.sample(size)
-
-        with torch.no_grad():
-            pos_label = torch.ones(size, dtype=torch.float, device=device)
-            neg_label = torch.zeros(size, dtype=torch.float, device=device)
 
         optimizer.zero_grad()
         tgan = tgan.train()
-        pos_prob, neg_prob = tgan.contrast(src_l_cut, dst_l_cut, dst_l_fake, ts_l_cut, NUM_NEIGHBORS)
-
-        loss = criterion(pos_prob, pos_label)
-        loss += criterion(neg_prob, neg_label)
+        pred_prob = tgan.contrast(src_l_cut, dst_l_cut, ts_l_cut, NUM_NEIGHBORS)
+        loss = criterion(pred_prob, torch.from_numpy(label_l_cut).float().to(device))
 
         loss.backward()
         optimizer.step()
@@ -306,20 +279,19 @@ for epoch in range(NUM_EPOCH):  # NUM_EPOCH = 50
         # get training results
         with torch.no_grad():
             tgan = tgan.eval()
-            pred_score = np.concatenate([(pos_prob).cpu().detach().numpy(), (neg_prob).cpu().detach().numpy()])
-            pred_label = pred_score > 0.5
-            true_label = np.concatenate([np.ones(size), np.zeros(size)])
+            pred_prob = pred_prob.cpu().detach().numpy()
+            pred_label = pred_prob > 0.5
+            true_label = label_l_cut
             acc.append((pred_label == true_label).mean())
-            ap.append(average_precision_score(true_label, pred_score))
+            ap.append(average_precision_score(true_label, pred_prob))
             f1.append(f1_score(true_label, pred_label))
             m_loss.append(loss.item())
-            auc.append(roc_auc_score(true_label, pred_score))
+            auc.append(roc_auc_score(true_label, pred_prob))
 
-    # validation phase use all informationrj
-
+    # validation phase use all information
     tgan.ngh_finder = full_ngh_finder
-    val_acc, val_ap, val_f1, val_auc = eval_one_epoch('val for old nodes', tgan, val_rand_sampler, val_src_l,
-                                                      val_dst_l, val_ts_l, val_label_l)
+    val_acc, val_ap, val_f1, val_auc = eval_one_epoch('val for old nodes', tgan, val_rand_sampler,
+                                                      val_src_l, val_dst_l, val_ts_l, val_label_l)
 
     # nn_val_acc, nn_val_ap, nn_val_f1, nn_val_auc = eval_one_epoch('val for new nodes', tgan, val_rand_sampler,
     #                                                               nn_val_src_l,
@@ -346,6 +318,7 @@ for epoch in range(NUM_EPOCH):  # NUM_EPOCH = 50
     else:
         if early_stopper.is_best:
             torch.save(tgan.state_dict(), get_checkpoint_path(epoch))
+            logger.info('Saved {}-TGAT-{}.pth'.format(MODEL_NUM, early_stopper.best_epoch))
             for i in range(epoch):
                 try:
                     os.remove(get_checkpoint_path(i))
